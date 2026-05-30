1cc94491a5b4355ed4e4e1f01fa866c6cecd3a1b
Harshith K <harshithkotian999@gmail.com>
2026-05-30 16:10:22 +0530

    chore: remove .env from tracking and add to .gitignore

--- sim_fail.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import { openDB } from 'idb';
-
-async function run() {
-  // Can't run idb easily from node without a browser environment.
-  console.log("To simulate, I can just write a quick button in OwnerStats to insert a fake failed transaction, or just verify the code.")
-}
-run();
